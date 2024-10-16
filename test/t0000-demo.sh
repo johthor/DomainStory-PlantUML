5912564dd3aa33570f7f02c708700840f8fdb267
Johannes Thorn <2544827+johthor@users.noreply.github.com>
2024-10-16 22:55:41 +0200

    Add Sharness to hopefully create some helpful automated tests

---
+++ test/t0000-demo.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# shellcheck disable=SC2034
+test_description='Show basic features of Sharness'
+
+. ./lib/sharness/sharness.sh
+
+test_expect_success 'Success is reported like this' '
+    echo hello world | grep hello
+'
+
+test_expect_success 'Commands are chained this way' '
+    test x = "x" &&
+    test 2 -gt 1 &&
+    echo success
+'
+
+test_expect_failure 'We expect this to fail' '
+    test 1 = 2
+'
+
+test_done
+
+# vi: set ft=sh.sharness :
