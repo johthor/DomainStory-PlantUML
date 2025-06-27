fd14cc3be1c6b6d820ed228ea8dabd02238dbddb
Johannes Thorn <2544827+johthor@users.noreply.github.com>
2025-06-27 21:04:53 +0200

    Split magick calls and diagram file handling

--- test/lib/testUtils.sh
+++ test/lib/testUtils.sh
@@ -13,58 +13,83 @@ ensureSnapshotExists() {
 }
 
 computeComparison() {
+  magick compare -metric AE -fuzz 5% \
+    "$1" \
+    "$2" \
+    -highlight-color Magenta \
+    magick "$3"
+}
+
+computeDiagramComparison() {
   subject="$1"
   fileSuffix="$2"
 
-  magick compare -metric AE -fuzz 5% \
+  computeComparison \
     "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_EXPECTATION$fileSuffix" \
     "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_ACTUAL$fileSuffix" \
-    -highlight-color Magenta \
-    magick "$SHARNESS_TEST_DIRECTORY/$subject"._COMPARISON.png
+    "$SHARNESS_TEST_DIRECTORY/$subject"._COMPARISON.png
 }
 
 computeComposite() {
+  magick composite \
+    "$1" \
+    "$2" \
+    -compose difference "$3"
+}
+
+computeDiagramComposite() {
   subject="$1"
   fileSuffix="$2"
 
-  magick composite \
+  computeComposite \
     "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_EXPECTATION$fileSuffix" \
     "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_ACTUAL$fileSuffix" \
-    -compose difference "$SHARNESS_TEST_DIRECTORY/$subject"._COMPOSITE.png
+    "$SHARNESS_TEST_DIRECTORY/$subject"._COMPOSITE.png
 }
 
 computeFlicker() {
+  magick -delay 100 \
+    "$1" \
+    "$2" \
+    -loop 0 "$3"
+}
+
+computeDiagramFlicker() {
   subject="$1"
   fileSuffix="$2"
 
-  magick -delay 100 \
+  computeFlicker \
     "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_EXPECTATION$fileSuffix" \
     "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_ACTUAL$fileSuffix" \
-    -loop 0 "$SHARNESS_TEST_DIRECTORY/$subject"._FLICKER.gif
+    "$SHARNESS_TEST_DIRECTORY/$subject"._FLICKER.gif
 }
 
 computeStats() {
-  subject="$1"
-  fileSuffix="$2"
-
   magick \
-    "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_EXPECTATION$fileSuffix" \
-    "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_ACTUAL$fileSuffix" \
+    "$1" \
+    "$2" \
     -compose Difference -composite \
     -colorspace gray -verbose  info: |\
     sed -n '/statistics:/,/^  [^ ]/ p' 1>&2 \
-    > "$SHARNESS_TEST_DIRECTORY/$subject"._STATS.txt
+    > "$3"
 }
 
-computeDiff() {
+computeDiagramStats() {
   subject="$1"
   fileSuffix="$2"
 
+  computeStats \
+    "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_EXPECTATION$fileSuffix" \
+    "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_ACTUAL$fileSuffix" \
+    "$SHARNESS_TEST_DIRECTORY/$subject"._STATS.txt
+}
+
+computeDiff() {
   values_small="^0\.[^ ]+ \([^e]+e-..\)"
   values_smaller="^[^e]+e-.. \([^e]+e-..\)"
   stat=$(magick compare -metric MAE \
-    "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_EXPECTATION$fileSuffix" \
-    "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_ACTUAL$fileSuffix" \
+    "$1" \
+    "$2" \
     null: 2>&1 | \
     grep -iv -e 'inkscape' -e '^$')
 
@@ -77,15 +102,24 @@ computeDiff() {
   fi
 }
 
+computeDiagramDiff() {
+  subject="$1"
+  fileSuffix="$2"
+
+  computeDiff \
+    "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_EXPECTATION$fileSuffix" \
+    "$SHARNESS_TEST_DIRECTORY/$subject$SNAPSHOT_ACTUAL$fileSuffix"
+}
+
 compareImages() {
   subject="$1"
   fileSuffix="$2"
 
-  if [ "$(computeDiff "$subject" "$fileSuffix")" != '0 (0)' ]; then
-    computeComparison "$subject" "$fileSuffix"
-    computeComposite "$subject" "$fileSuffix"
-    computeFlicker "$subject" "$fileSuffix"
-    computeStats "$subject" "$fileSuffix"
+  if [ "$(computeDiagramDiff "$subject" "$fileSuffix")" != '0 (0)' ]; then
+    computeDiagramComparison "$subject" "$fileSuffix"
+    computeDiagramComposite "$subject" "$fileSuffix"
+    computeDiagramFlicker "$subject" "$fileSuffix"
+    computeDiagramStats "$subject" "$fileSuffix"
 
     echo "compare the files for $subject" 1>&2
     test 1 != 1
